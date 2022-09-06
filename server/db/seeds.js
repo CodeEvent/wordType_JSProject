7559ba33e4b263c71dd31c84dd2a690a1a7ccf3a
pythearn <ermandmani@gmail.com>
2022-09-06 11:04:30 +0100

    DB added

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,19 +1,37 @@
 use games_hub;
 db.dropDatabase();
 
-db.games.insertTypedWord([
+db.games.insertMany([
   {
-    typedWord: "Love Letter",
-    },
-  {
-    typedWord: "Catan",
- 
-    },
-  {
-    typedWord: "Exploding Kittens",
-
-    },
-  {
-    typedWord: "Resistance",
+    name: "Love Letter",
+    playingTime: 20,
+    players: {
+      min: 2,
+      max: 4
     }
+  },
+  {
+    name: "Catan",
+    playingTime: 90,
+    players: {
+      min: 3,
+      max: 4
+    }
+  },
+  {
+    name: "Exploding Kittens",
+    playingTime: 15,
+    players: {
+      min: 2,
+      max: 5
+    }
+  },
+  {
+    name: "Resistance",
+    playingTime: 30,
+    players: {
+      min: 5,
+      max: 10
+    }
+  }
 ]);
